945b4538e2840b202d046987eaca4b995f7e2266
Andy Wu <andy.wu@yale.edu>
2023-08-24 21:43:09 -0700

    add build folder

---
+++ build/precache-manifest.d37fbfbe61d84f953efebdc9db7efad1.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418ec1ecc30e94150ccf11ce99aad383",
+    "url": "./index.html"
+  },
+  {
+    "revision": "54ebea7f9ee4a4ce66ff",
+    "url": "./static/css/2.4439b770.chunk.css"
+  },
+  {
+    "revision": "382b830d9fe8237ec7da",
+    "url": "./static/css/main.35508eea.chunk.css"
+  },
+  {
+    "revision": "54ebea7f9ee4a4ce66ff",
+    "url": "./static/js/2.234f89a0.chunk.js"
+  },
+  {
+    "revision": "d73471e9d2c7a5484bced7c029b545b5",
+    "url": "./static/js/2.234f89a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382b830d9fe8237ec7da",
+    "url": "./static/js/main.1c2171e9.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "f668a9b38f3f803fbded4a25d4d5a410",
+    "url": "./static/media/blog.f668a9b3.png"
+  },
+  {
+    "revision": "79cd9f760e60e350befbf1f7739e2ead",
+    "url": "./static/media/colorgame.79cd9f76.png"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "./static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c74ca79014344b4b915b44ff63c25906",
+    "url": "./static/media/image1.c74ca790.jpg"
+  },
+  {
+    "revision": "8de6195d8faafe31ac0a842eb0cbe1a9",
+    "url": "./static/media/image2.8de6195d.jpg"
+  },
+  {
+    "revision": "28470933ba170d7cd8d5495969722a5d",
+    "url": "./static/media/image3.28470933.jpg"
+  },
+  {
+    "revision": "0b37f43494ce5a35f3e1575612048750",
+    "url": "./static/media/image4.0b37f434.jpg"
+  },
+  {
+    "revision": "8d16c4aff0e7bd3db1b8bab9619aea9c",
+    "url": "./static/media/image5.8d16c4af.jpg"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "./static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "./static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "./static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "./static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "./static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "6726353fb386d523b7cb2c50a4d8b966",
+    "url": "./static/media/joinus.6726353f.png"
+  },
+  {
+    "revision": "07d08791278e5683a9b8327f4ff2fe27",
+    "url": "./static/media/localscale_pic.07d08791.png"
+  },
+  {
+    "revision": "f8cf62a34cb079dae19f7869e8f374d8",
+    "url": "./static/media/male.f8cf62a3.png"
+  },
+  {
+    "revision": "f8cf62a34cb079dae19f7869e8f374d8",
+    "url": "./static/media/male1.f8cf62a3.png"
+  },
+  {
+    "revision": "6d453631dd274024381362a306437cb2",
+    "url": "./static/media/musicplayer.6d453631.png"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "./static/media/myImage.bd80794f.png"
+  },
+  {
+    "revision": "b427734de3a0598e92a8765365f798fb",
+    "url": "./static/media/newsletter.b427734d.png"
+  },
+  {
+    "revision": "7a9290d7bcdc0c3742f1475a05994e5c",
+    "url": "./static/media/predictai_pic.7a9290d7.png"
+  },
+  {
+    "revision": "9a76004038cb5b7bba1bf3b8f01fa7ce",
+    "url": "./static/media/simmer_pic.9a760040.png"
+  },
+  {
+    "revision": "ea42b91f720135121a4328bdd151aea9",
+    "url": "./static/media/todolist.ea42b91f.png"
+  }
+]);
